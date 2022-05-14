1f3d1d6049bfb37147554c4858b05e17c47e1422
mustafa mohammed <mustafamohammed@mustafas-MacBook-Pro.local>
2022-05-14 16:00:23 +0100

    projects added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+      <About />
+      <Skills />
+      <Projects />
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,6 @@ import { HiArrowNarrowRight } from "react-icons/hi";
 const Home = () => {
   return (
     <div name="home" className="w-full h-screen bg-[#0a192f]">
-      {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-white">Hi, my name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
