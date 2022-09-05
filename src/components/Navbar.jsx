7c2f29c399db8076d073f7f9bb99169e9251e28c
mustafa mohammed <mustafamohammed@mustafas-MacBook-Pro.local>
2022-09-05 14:53:36 +0100

    final changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -107,22 +107,6 @@ const Navbar = () => {
               Github <FaGithub size={30} />
             </a>
           </li>
-          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300  bg-[#0a192f]">
-            <a
-              className="flex justify-between items-center w-full text-gray-300"
-              href="/https://github.com/mustafamohamed-web?tab=repositories"
-            >
-              Email <FaEnvelope size={30} />
-            </a>
-          </li>
-          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300  bg-[#0a192f]">
-            <a
-              className="flex justify-between items-center w-full text-gray-300"
-              href="file.doc"
-            >
-              Resume <BsFillPersonLinesFill size={30} />
-            </a>
-          </li>
         </ul>
       </div>
     </div>
